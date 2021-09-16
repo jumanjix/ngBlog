f09b4e3aabd21b9a0866cd5237ec1e4250df1b93
Holivares <58671689+Holivares@users.noreply.github.com>
2021-09-16 13:02:26 +0200

    INVIO

---
+++ src/app/posts/posts-list/posts.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Post } from '../interfaces/post';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  public title = 'Lista dei posti'
+
+  
+  public showBadge: boolean = false;
+
+  posts : Post[] = [];
+
+  public togleIsnewBadge(): void{
+    this.showBadge = !this.showBadge;
+  }
+
+  constructor(private apiService : ApiService) { 
+    this.apiService.getPosts().subscribe( data => {
+      this.posts = data;
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
