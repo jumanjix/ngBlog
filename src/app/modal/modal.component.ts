fbed5827185d9e117b32f489749056c95f42d5d2
jmfx <jumanji.felix@gmail.com>
2021-09-16 12:38:10 +0200

    caricamento propic e controllo su chiusura modal

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -10,38 +10,37 @@ import { User } from '../interfaces/user';
 })
 export class ModalComponent implements OnInit {
 
-  @Input() user! : User;
-  @Input() operation! : 'create' | 'edit' | 'delete';
+  @Input() user!: User;
+  @Input() operation!: 'create' | 'edit' | 'delete';
 
-  idCount! : number;
+  idCount!: number;
 
-  constructor(public activeModal : NgbActiveModal, private apiService : ApiService) { }
+  constructor(public activeModal: NgbActiveModal, private apiService: ApiService) { }
 
   ngOnInit(): void {
     console.log("modal component : ngOnInit start");
 
     // in caso di Nuovo Utente creo un modello per il form
     if (this.operation === 'create') {
-      console.log('creating new user model ...');
-      
-      /* // get sugli utenti per definire id del nuovo utente
-      this.apiService.getUsers().subscribe( result => {
+      console.log('creating new-user model ...');
+
+      // get sugli utenti per definire id del nuovo utente
+      this.apiService.getUsers().subscribe(result => {
         //console.log(result.length);
         //this.idCount = result.length + 1;
         // giocare con la lunghezza dell'array utenti crea problemi di sfalsamento quando viene 
         // cancellato un utente e poi aggiunto un altro
         // un intero random abbastanza grande non dovrebbe creare problemi per poche operazioni
-            
-      }) */
-
-      // creo id random tra 21 e 200
-      this.idCount = Math.floor( Math.random() * (200 - 21) + 21 );
-      console.log('ID: ' + this.idCount);  
+        
+        // creo id random compreso tra lunghezza array utenti e 200
+        this.idCount = Math.floor(Math.random() * (200 - result.length) + result.length);
+        console.log('ID: ' + this.idCount);
+      });
 
       // modello del nuovo utente
       this.user = {
-        nome : '',
-        cognome : '',
+        nome: '',
+        cognome: '',
         username: '',
         email: '',
         occupazione: '',
@@ -49,23 +48,24 @@ export class ModalComponent implements OnInit {
         followers: 0,
         id: 0 // TODO : provare ad assegnare il valore di idCount qui
       }
-    } 
-    
+    }
+
   }
 
-  onOperationConsent() {    
-    console.log("form submitted");
-    
+  // chiamata quando si decide di procedere con l'operazione di creazione/modifica/eliminazione
+  onOperationConsent() {
+    console.log("form submitted with values:");
+    console.log(this.user);
+
     if (this.operation === 'create') { // se si crea un nuovo utente
 
       this.user.id = this.idCount; // set dell'id del nuovo utente
-      console.log(this.user);      
 
-     /*  this.apiService.postUser(this.user).subscribe( result => {
-        console.log(result);
-        
-      }); */
-      
+      /*  this.apiService.postUser(this.user).subscribe( result => {
+         console.log(result);
+         
+       }); */
+
     } /* else if (this.operation === 'edit') { // se si modifica un utente
       console.log('edit user mode on');
       
@@ -81,7 +81,7 @@ export class ModalComponent implements OnInit {
       });      
     } */
 
-    // ritorno l'utente creato/modificato/eliminato allo users component
-    this.activeModal.close(this.user);   
+    // alla chiusura della modal, ritorna al componente Users l'utente creato/modificato/eliminato
+    this.activeModal.close(this.user);
   }
 }
